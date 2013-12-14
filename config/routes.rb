9aaae630d1129576f7e8fafdc9e174fd778d1554
Codruț Constantin Gușoi <codrut_sdw@yahoo.com>
2013-12-14 12:34:01 +0200

    let me pull lol #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rosap::Application.routes.draw do
   
+  root to: 'static_pages#home'
 
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
 
 
