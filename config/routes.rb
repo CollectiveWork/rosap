63c72998b839394cd917f088a2989a9c1561e469
Butnaru Andrei-Madalin <butnaruandreimadalin@gmail.com>
2013-12-14 11:42:02 +0200

    Initial commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rosap::Application.routes.draw do
   
+
+
+
+
+
+
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
