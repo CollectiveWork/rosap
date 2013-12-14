3ea74f3905ccad1fc556275ae0aa2b8a2edee715
Butnaru Andrei-Madalin <butnaruandreimadalin@gmail.com>
2013-12-14 11:47:13 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rosap::Application.config.secret_key_base = '375cf5d791dea9c361f102a942d87f72b5aca8dcac739b507fd367bf786413f8ffe31d4a178bd94c268f8c687200c11422ade9876612369df3c7cea321f10050'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rosap::Application.config.session_store :cookie_store, key: '_rosap_session'
