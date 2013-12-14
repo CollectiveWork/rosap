4fad0bed04e0545920ba97e6382eabe9da578de6
Codruț Constantin Gușoi <codrut_sdw@yahoo.com>
2013-12-14 13:23:51 +0200

    Finished facebook authentification #1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214102208) do
+ActiveRecord::Schema.define(version: 20131214104747) do
 
   create_table "github_users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,15 @@ ActiveRecord::Schema.define(version: 20131214102208) do
 
   add_index "github_users", ["email"], name: "index_github_users_on_email", unique: true
   add_index "github_users", ["reset_password_token"], name: "index_github_users_on_reset_password_token", unique: true
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
